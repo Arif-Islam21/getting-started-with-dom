2a3278fb54aeafa09f3f00efc5e6735d813a1156
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-10 11:12:39 +0600

    learned about classlist

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const lies = document.querySelectorAll(".places-to-go li");
-console.log(lies.innerText);
+const sections = document.querySelectorAll("section");
+// console.log(sections);
+for (const section of sections) {
+  section.style.border = "2px solid steelblue";
+  section.style.marginBottom = "5px";
+  section.style.borderRadius = "15px";
+  section.style.padding = "10px";
+  section.style.backgroundColor = "lightgray";
+}
+
+const placeContainer = document.getElementById("placeContainer");
+placeContainer.classList.add("text-violet");
